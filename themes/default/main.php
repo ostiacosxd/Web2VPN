c1fa9e7cedaffc3b9dabb07b94f26a2fdf5e917c
cuidabrutos <cuidabrutospvp@gmail.com>
2017-01-23 03:18:01 -0600

    Update main.php

--- themes/default/main.php
+++ themes/default/main.php
@@ -2,192 +2,154 @@
 
 <html>
 <head>
-<title><!--[site_name]--></title>
+<title>VPN Service</title>
 <meta name="description" content="<!--[meta_description]-->">
 <meta name="keywords" content="<!--[meta_keywords]-->">
 <style type="text/css">
-	/* GLOBAL ELEMENTS */
-	html,body {
-		margin: 0; padding: 0;
-		background: #0b1933;
-		font-size: 100%;
-		font-family: "trebuchet ms", arial, verdana;
-		color: #444;
-		text-align: center;
-	}
-	a {
-		color: #EF7B0A;
-		text-decoration:none; 
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-	p {
-		color: #444;
-		line-height: 170%; margin: 5px 0;
-	}
-	p, td, th, ul {
-		font-size: 80%;
-	}
-	
-	/* LAYOUT */
-	#wrapper {
-		width: 700px;
-		margin: 0 auto 0 auto;
-		text-align: left;
-	}
-	
-	#content {
-		background: #fff;
-		border-top: 3px solid #ce6c1c;
-		border-bottom: 3px solid #ce6c1c;
-		padding: 20px;
-	}
-	
-	/* ELEMENTS */
-	h1 {
-		font: 250% "trebuchet ms";
-		color: #fff;
-		padding: 40px 0 10px 10px;
-		margin: 0;
-	}
-	h1 span {
-		color: #6BAD42;
-	}
-	h1 a {
-		color: #FFFFFF;
-	}
-	h1 a:hover {
-		color: #6BAD42;
-		text-decoration: none;
-	}
-	h1 a:hover span {
-		color: #FFFFFF;
-	}
-	h2 {
-		font: bold 100% arial, verdana, sans-serif;
-		color: #3B578B;
-		border-bottom: 1px solid #ccc;
-		padding-bottom: 3px;
-		margin: 25px 0 10px 0;
-	}
-	p+p {
-		padding-top: 1em;
-	}
-	form.form {
-		font-size: 80%;
-		background-color: #f5f5f5;
-		padding: 10px;
-	}
-	#options {
-		list-style-type: none;
-		width: 500px;
-		margin: 10px; padding: 0;
-	}
-	#options li { 
-		float: left;
-		width: 240px;
-		border-left: 5px solid #ccc;
-	}
-	#footer {
-		margin: 10px 0 0 0; 
-		font-size: 80%;
-		color: #ccc;
-	}
-	#nav {
-		text-align: right;
-		list-style-type: none;
-		font-size: 80%;
-		border-top: 1px solid #ccc;
-		margin: 20px 0 0 0;
-		padding: 0;
-	}
-	#nav li {
-		padding: 0 5px 0 5px;
-		display: inline;
-		border-left: 1px solid #ccc;
-	}
-	.left {
-		float: left;
-	}
-	
-	/* STYLES */
-	.first {
-		margin-top: 0;
-	}
-	input.textbox {
-		width: 500px;
-		font: 120% arial, verdana, sans-serif;
-	}
-	input.button {
-		margin-top: 10px;
-		font-family: arial, verdana, sans-serif;
-		font-size: 120%;
-	}
-	label {
-		font-weight: light;
-	}
-	#error {
-		border: 1px solid red;
-		border-left: 5px solid red;
-		padding: 2px;
-		margin: 5px 0 15px 0;
-		background: #eee;
-	}
-	
-	/* TABLES USED IN COOKIE MANAGEMENT / EDIT BROWSER PAGES */
-	table {
-		border-color: #666;
-		border-width: 0 0 1px 1px;
-		border-style: solid;
-		width: 50%;
-	}
-	th {
-		font-size: normal;
-		background: #ccc;
-		border-width: 2px;
-	}
-	td, th {
-		border-color: #666;
-		border-width: 1px 1px 0 0;
-		border-style: solid;
-		padding: 2px 10px 2px 10px;
-	}
-	td {
-		background-color: #EEEEEE;
-	}
-	.full-width {
-		width: 98%;
-	}
-	.large-table { 
-		width: 75%; 
-		margin-top: 15px;
-	}
-	.large-table td, .large-table th {
-		padding: 5px;
-	}
-	td.small-note {
-		font-size: 60%;
-		padding: 2px;
-		text-align: right;
-	}
-	
-	/* TOOLTIP HOVER EFFECT */
-	label {
-		font-weight: bold;
-		line-height: 20px;
-		cursor: help;
-	}
-	#tooltip {
-		width: 20em;
-		color: #fff;
-		font-size: 12px;
-		font-weight: normal;
-		padding: 5px;
-		border: 3px solid #333;
-		text-align: left;
-		background-color: #555555;
-	}
+@import url(https://fonts.googleapis.com/css?family=Roboto:400,300,600,400italic);
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  -o-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+body {
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  font-weight: 100;
+  font-size: 12px;
+  line-height: 30px;
+  color: #777;
+  background: #4CAF50;
+}
+
+.container {
+  max-width: 400px;
+  width: 100%;
+  margin: 0 auto;
+  position: relative;
+}
+
+#contact input[type="text"],
+#contact input[type="email"],
+#contact input[type="tel"],
+#contact input[type="url"],
+#contact textarea,
+#contact button[type="submit"] {
+  font: 400 12px/16px "Roboto", Helvetica, Arial, sans-serif;
+}
+
+#contact {
+  background: #F9F9F9;
+  padding: 25px;
+  margin: 150px 0;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+
+#contact h3 {
+  display: block;
+  font-size: 30px;
+  font-weight: 300;
+  margin-bottom: 10px;
+}
+
+#contact h4 {
+  margin: 5px 0 15px;
+  display: block;
+  font-size: 13px;
+  font-weight: 400;
+}
+
+fieldset {
+  border: medium none !important;
+  margin: 0 0 10px;
+  min-width: 100%;
+  padding: 0;
+  width: 100%;
+}
+
+#contact input[type="text"],
+#contact input[type="email"],
+#contact input[type="tel"],
+#contact input[type="url"],
+#contact textarea {
+  width: 100%;
+  border: 1px solid #ccc;
+  background: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+}
+
+#contact input[type="text"]:hover,
+#contact input[type="email"]:hover,
+#contact input[type="tel"]:hover,
+#contact input[type="url"]:hover,
+#contact textarea:hover {
+  -webkit-transition: border-color 0.3s ease-in-out;
+  -moz-transition: border-color 0.3s ease-in-out;
+  transition: border-color 0.3s ease-in-out;
+  border: 1px solid #aaa;
+}
+
+#contact textarea {
+  height: 100px;
+  max-width: 100%;
+  resize: none;
+}
+
+#contact button[type="submit"] {
+  cursor: pointer;
+  width: 100%;
+  border: none;
+  background: #4CAF50;
+  color: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+  font-size: 15px;
+}
+
+#contact button[type="submit"]:hover {
+  background: #43A047;
+  -webkit-transition: background 0.3s ease-in-out;
+  -moz-transition: background 0.3s ease-in-out;
+  transition: background-color 0.3s ease-in-out;
+}
+
+#contact button[type="submit"]:active {
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
+}
+
+.copyright {
+  text-align: center;
+}
+
+#contact input:focus,
+#contact textarea:focus {
+  outline: 0;
+  border: 1px solid #aaa;
+}
+
+::-webkit-input-placeholder {
+  color: #888;
+}
+
+:-moz-placeholder {
+  color: #888;
+}
+
+::-moz-placeholder {
+  color: #888;
+}
+
+:-ms-input-placeholder {
+  color: #888;
+}
 </style>
 <?=injectionJS();?>
 <script type="text/javascript">
@@ -199,62 +161,17 @@
 </script>
 </head>
 <body>
-<div id="wrapper">
-	<div id="header">
-		<h1><a href="index.php"><?php
-			# Just a bit of PHP to auto-color a multiple word name
-			global $themeReplace;
-			if (isset($themeReplace['site_name'])) {
-				$wc=0;
-				$words = explode(' ', $themeReplace['site_name']);
-				foreach ($words as $word) {
-					$wc++;
-					if ($wc%2==1) {
-						echo $word.' ';
-					} else {
-						echo '<span>'.$word.'</span> ';
-					}
-				}
-			}
-		?></a></h1>
-	</div>
-	<div id="content">
-
-		<!-- CONTENT START -->
-
-		<!--[error]-->
-
-		<h2 class="first">About</h2>
-
-		<!--[description]-->
-
-		<h2>Enter URL</h2>
-
-		<!--[index_above_form]-->
-
-		<form action="includes/process.php?action=update" method="post" onsubmit="return updateLocation(this);" class="form">
-			<input type="text" name="u" id="input" size="40" class="textbox">
-			<input type="submit" value="Go" class="button"> &nbsp; [<a style="cursor:pointer;" onclick="document.getElementById('options').style.display=(document.getElementById('options').style.display=='none'?'':'none')">options</a>]
-			<ul id="options">
-				<?php foreach ($toShow as $option) echo '<li><input type="checkbox" name="'.$option['name'].'" id="'.$option['name'].'"'.$option['checked'].'><label for="'.$option['name'].'" class="tooltip" onmouseover="tooltip(\''.$option['escaped_desc'].'\')" onmouseout="exit();">'.$option['title'].'</label></li>';?>
-			</ul>
-			<br style="clear: both;">
-		</form>
-	 
-		<!--[index_below_form]-->
-
-		<!-- CONTENT END -->
-	 
-		<ul id="nav">
-			<li class="left"><a href="index.php">Home</a></li>
-			<li class="left"><a href="edit-browser.php">Edit Browser</a></li>
-			<li class="left"><a href="cookies.php">Manage Cookies</a></li>
-			<li><a href="disclaimer.php">Disclaimer</a></li>            
-		</ul>
-	</div>
-	<div id="footer">
-		Powered by <a href="https://www.ihoster.us/">Web2VPN</a>&reg; <!--[version]-->.
-	</div>
+<div class="container">  
+  <form action="includes/process.php?action=update" id="contact" method="post" onsubmit="return updateLocation(this);" class="form">
+    <h3>VPN/Proxy Service</h3>
+    <fieldset>
+      URL: <input type="text" name="u" id="input" size="40" class="textbox">
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit">Open</button>
+    </fieldset>
+    <p class="copyright">Designed by <a href="https://www.ihoster.us" target="_blank" title="Cuidabrutos">Cuidabrutos</a></p>
+  </form>
 </div>
 </body>
 </html>
