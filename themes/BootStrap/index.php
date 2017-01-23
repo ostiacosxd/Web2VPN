901d6b3f40222d350b2c44b61efcd3b9278924d8
cuidabrutos <cuidabrutospvp@gmail.com>
2017-01-23 03:25:09 -0600

    Create index.php

---
+++ themes/BootStrap/index.php
@@ -0,0 +1,9 @@
+<?php
+/*******************************************************************
+* Web2VPN is copyright and trademark created by cuidabrutos, Inc. d/b/a Web2VPN
+* and/or its licensors, successors and assigners. All rights reserved.
+*
+* Use of Web2VPN is subject to the terms of the Software License Agreement.
+******************************************************************/
+header('Location: /../../index.php');
+?>
